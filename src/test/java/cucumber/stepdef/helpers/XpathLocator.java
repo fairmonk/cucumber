783457152e598c18d2118b8002ba06c404ac1d0a
mihai onoprienco <fairmonk@gmail.com>
2014-10-22 21:48:46 +0300

    New class added to keep a list of xpath locators

---
+++ src/test/java/cucumber/stepdef/helpers/XpathLocator.java
@@ -0,0 +1,19 @@
+package cucumber.stepdef.helpers;
+
+/**
+ * Created by misha on 10/22/2014.
+ */
+public class XpathLocator {
+    public final static String megadeals = "//img[starts-with(@alt,'Check out this weeks picks')]";
+    public final static String mybag = "//a[@id='aBagLink']";
+    public final static String item1 = "//img[@alt='Nike Fundamentals Full Zip Hoody Mens']";
+    public final static String item2 = "//span[@id='dnn_ctr51055_BrowseV3View_lstProducts_hypProductName_21']";
+    public final static String sizeDropDown = "//div/select[@id='sizeDdl']";
+    public final static String addToBag = "//a[@id='aAddToBag']";
+    public final static String quantity = "//span[@id='bagQuantity']";
+    public final static String number = "//img[@alt='Add one item']";
+    public final static String updateBag = "//a[text()='Update bag']";
+
+    public final static String sizeElementText = "Small";
+
+}
